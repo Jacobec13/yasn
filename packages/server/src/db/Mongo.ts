0547d025401e8e20f1ffe257a8a8265636fd5117
Daniil Kundik <kundik.d.vi@sberbank.ru>
2021-05-04 16:23:08 +0300

    add mongo

---
+++ packages/server/src/db/Mongo.ts
@@ -0,0 +1,40 @@
+import {MongoClient, Db} from 'mongodb';
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'yasn';
+
+interface GlobalExtend extends NodeJS.Global {
+	mongoClient: Mongo;
+}
+
+declare var global: GlobalExtend;
+
+export class Mongo {
+	private nativeMongoClient: MongoClient | null;
+	private db: Db | null;
+
+	private constructor() {
+		this.nativeMongoClient = null;
+		this.db = null;
+	}
+
+	static async getInstance(): Promise<Mongo> {
+		if (!global.mongoClient) {
+			global.mongoClient = new Mongo();
+			await global.mongoClient.init();
+		}
+
+		return Promise.resolve(global.mongoClient);
+	}
+
+	private async init() {
+		this.nativeMongoClient = new MongoClient(url);
+		await this.nativeMongoClient.connect();
+		this.db = this.nativeMongoClient.db(dbName);
+	}
+
+	public getDb() {
+		return this.db!;
+	}
+}
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import './db/Mongo';
+
 const app = express();
 const port = 3000;
 
